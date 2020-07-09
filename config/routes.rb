00a737ebe9194174fff845f0e16c42abd7fd62f5
nobu-tech64 <douji533rikou@icloud.com>
2020-07-09 11:45:59 +0900

    削除ボタン作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "notes#index"
-  resources :notes, only: [:index, :new, :create, :show, :edit, :update]
+  resources :notes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
