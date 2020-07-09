8482a8e214b03de9882de7d932c44d0dd64bd078
nobu-tech64 <douji533rikou@icloud.com>
2020-07-09 19:31:03 +0900

    日記の一覧を表示させるための準備

---
+++ app/assets/javascripts/list.js
@@ -0,0 +1,20 @@
+function existingDiaryList(){
+  $(".mainContainer").click(function(){
+    let newElement = document.createElement("p");
+    let newContent = document.createTextNode("test");
+    newElement.appendChild(newContent);
+    newElement.setAttribute("id", "child-p1");
+
+    // 「日記を書く」ボタンがあるのでまず削除
+    if (document.getElementById("newdiary") != null) {
+      document.getElementById("newdiary").remove();
+    }
+
+    let parentDiv = document.getElementById("existing_diary");
+
+    parentDiv.appendChild(newElement);
+  });
+
+}
+
+window.addEventListener("load", existingDiaryList);
