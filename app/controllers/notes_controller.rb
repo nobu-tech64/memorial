6a228e3d6db63e793d027279b7d8c4cb99572cb6
nobu-tech64 <douji533rikou@icloud.com>
2020-07-27 18:03:50 +0900

    修正

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,9 +2,6 @@ class NotesController < ApplicationController
   def index
     @notes = Note.all
     @user = User.all
-    if true then
-      logger.debug("if文の中に入りました")
-    end
   end
 
   def new
